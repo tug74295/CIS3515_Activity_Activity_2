3fa792551d6aeea7019a8a8a9d5fa44bfb61cefc
tug74295 <tug74295@temple.edu>
2025-02-20 12:17:13 -0500

    Fixed TODOs to implement font change

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,9 +1,11 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class DisplayActivity : AppCompatActivity() {
 
@@ -14,12 +16,19 @@ class DisplayActivity : AppCompatActivity() {
     private lateinit var lyricsDisplayTextView: TextView
     private lateinit var textSizeSelectorButton: Button
 
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        lyricsDisplayTextView.textSize = it.resultCode.toFloat()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
+
+        textSizeSelectorButton.setOnClickListener  {
+            launcher.launch(Intent(this@DisplayActivity, TextSizeActivity::class.java))
+        }
 
     }
 }
